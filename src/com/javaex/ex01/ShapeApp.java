c602196b7fdb749e33322060479dab220d817f3d
rhs0402 <rhs0402@naver.com>
2024-01-10 17:59:22 +0900

    2024-01-10 추상화, 인터페이스

---
+++ src/com/javaex/ex01/ShapeApp.java
@@ -0,0 +1,24 @@
+package com.javaex.ex01;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+
+		//Ractangle class의 ShowInfo -> draw 수정
+		//s는 abstract인 Shape으로 메모리에 올릴 수 없음
+		//sr1의 가로크기를 가져오기 위해 getter 메소드 생성
+		
+		//Shape s = new Shape("빨강");
+		Circle c1 = new Circle("녹색", 10);
+		Ractangle r1 = new Ractangle("빨강", 4, 4);
+		Shape sr1 = new Ractangle("빨강", 12, 10);
+		
+		//sr1을 그리는 메소드 호출
+		sr1.draw();
+		//sr1의 가로크기 출력
+		System.out.println("sr1의 가로는 " + ((Ractangle)sr1).getWidth() + "입니다."	);
+	}
+}
+
+	
+	
---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,27 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+    private String name;
+
+	protected  String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	 public void showName() {
+		    
+	 }
+	 
+	 public void sing() {
+		 
+	 }
+	 
+	 public void fly() {
+		 
+	 }
+
+
+}
